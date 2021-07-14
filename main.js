c6143c2c920b84fc54ac7202979e57731bd3dde3
null <null>
2021-07-14 13:08:36 +0100

    Basic Function

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 /* 
 👉 Write your kata here!
+
+You are trying to work out what your maximum efficiency would be when doing squats for an amount of time. 
+You have worked out that it takes you 3 seconds to do a full squat. 
+You would like to know how many Squats you can do if you complete the set without breaks. 
+
+
+👉 Write the function your CodeWarriors will start with below here: 
 */
 
-//👉 Write the function your CodeWarriors will start with below here:
+function maximumSquatAmount(time, singleSquatTime) {
+
+}
+
+module.exports = maximumSquatAmount;
--- main.test.js
+++ main.test.js
@@ -1,1 +1,32 @@
+const maximumSquatAmount = require("./main");
+
 //👉 Write your tests below here:
+test(`maximumSquatAmount returns the maximum amount of Squats the player can do in 120 seconds'`, () => {
+  // amount of time
+
+  const time = 120;
+  const singleSquatTime = 3;
+  const actual = maximumSquatAmount(time, singleSquatTime);
+  const expected = 40;
+  expect(actual).toBe(expected);
+});
+
+test(`maximumSquatAmount returns the maximum amount of Squats the player can do in 300 seconds if a squat takes 3 seconds'`, () => {
+  // amount of time
+
+  const time = 300;
+  const singleSquatTime = 3;
+  const actual = maximumSquatAmount(time, singleSquatTime);
+  const expected = 100;
+  expect(actual).toBe(expected);
+});
+
+test(`maximumSquatAmount returns the maximum amount of Squats the player can do in 300 seconds if a squat takes 5 seconds'`, () => {
+  // amount of time
+
+  const time = 300;
+  const singleSquatTime = 5;
+  const actual = maximumSquatAmount(time, singleSquatTime);
+  const expected = 60;
+  expect(actual).toBe(expected);
+});
